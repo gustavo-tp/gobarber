f54ca5bd9f56ce51c97992c8fbaa837b60480b76
Gustavo Turra Perandré <gustavo1190.gt@gmail.com>
2020-07-28 01:18:21 -0300

    Updates

---
+++ precache-manifest.3ae87f01d6343df20a41fb1cc79c9d68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3dc3e6c0d03908515fb233ef705aae",
+    "url": "/gobarber/index.html"
+  },
+  {
+    "revision": "d59697c32fbdd4080c46",
+    "url": "/gobarber/static/js/2.ba47fec4.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/gobarber/static/js/2.ba47fec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "163f77bdab5a404b4f55",
+    "url": "/gobarber/static/js/main.d24ba08f.chunk.js"
+  },
+  {
+    "revision": "fef7b95840843c931117",
+    "url": "/gobarber/static/js/runtime-main.3976d41c.js"
+  },
+  {
+    "revision": "cb3c3e0d86c1fc98cdf6b403ff07ff76",
+    "url": "/gobarber/static/media/logo.cb3c3e0d.svg"
+  },
+  {
+    "revision": "6eac314f78fe224c4d769fb646523773",
+    "url": "/gobarber/static/media/sign-in-background.6eac314f.png"
+  },
+  {
+    "revision": "c4d8f988b9c6499d56702a398b2f3d8b",
+    "url": "/gobarber/static/media/sign-up-background.c4d8f988.png"
+  }
+]);
