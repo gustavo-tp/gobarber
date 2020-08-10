7bb86e6cc010a2cb841360160fb2617ef86f15ae
Gustavo Turra Perandré <gustavo1190.gt@gmail.com>
2020-08-10 08:45:46 -0300

    Add icon and splash screen configuration to mobile

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,22 +1,31 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppProvider from './hooks';
 
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#321e38" />
-    <AppProvider>
-      <View style={{ flex: 1, backgroundColor: '#321e38' }}>
-        <Routes />
-      </View>
-    </AppProvider>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    // Verificar se usuário está logado
+
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#321e38" />
+      <AppProvider>
+        <View style={{ flex: 1, backgroundColor: '#321e38' }}>
+          <Routes />
+        </View>
+      </AppProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
